4504af2f082352e95bc46561e7911b3d31323365
kevin.pelaez <kevin.stid.pelaez@gmail.com>
2024-11-13 01:48:16 -0500

    adding envs in dockerfile

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -2,8 +2,8 @@ const baseUrlApi = process.env.NEXT_PUBLIC_API_URL_BACK_AUTH
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
-  try {
-    const plainCredentials = await req.json()
+
+  const plainCredentials = await req.json()
     console.log(
       "credenciales desde back del server credentials: ",
       plainCredentials
@@ -27,9 +27,11 @@ export async function POST(req) {
     const responseData = await response.json()
     console.log("responseData:", responseData)
     return Response.json(responseData)
-  } catch (error) {
-    console.log("Entro en el catch")
-    return new NextResponse("Internal server error",{status:500})
+  // try {
+    
+  // } catch (error) {
+  //   console.log("Entro en el catch")
+  //   return new NextResponse("Internal server error",{status:500})
    
-  }
+  // }
 }
